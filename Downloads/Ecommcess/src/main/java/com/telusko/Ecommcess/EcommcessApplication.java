b7c0d68e3c00d602e569eadcbde38b2995f258eb
RamnarayanMandal <ramnarayan847231@gmail.com>
2024-10-03 00:40:05 +0530

    frist commit

---
+++ Downloads/Ecommcess/src/main/java/com/telusko/Ecommcess/EcommcessApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.Ecommcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommcessApplication.class, args);
+	}
+
+}
---
+++ Downloads/Ecommcess/src/test/java/com/telusko/Ecommcess/EcommcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telusko.Ecommcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
